bcfa1918231e77cb78884ecf2e786d3469b011a0
noahroemer <noahlex3@gmail.com>
2026-08-22 22:43:42 -0500

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+
+        # Let's go backwards
+        jump = 0
+        target = len(nums) - 1
+
+        for i in range(len(nums)-2, -1, -1):
+            jump += 1
+            if nums[i] >= jump:
+                target -= jump
+                jump = 0
+            else:
+                continue
+        return True if target == 0 else False
+                
+            
+        
+        
